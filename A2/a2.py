31890b788ca721725435cba113efe3165ee8df48
rohxnsxngh <98181667+rohxnsxngh@users.noreply.github.com>
2023-09-12 11:49:31 -0500

    remove unused imports

--- A2/a2.py
+++ A2/a2.py
@@ -1,10 +1,8 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import cross_val_score, train_test_split, GridSearchCV
+from sklearn.model_selection import cross_val_score
 from sklearn.linear_model import LinearRegression, Ridge
 from sklearn.preprocessing import PolynomialFeatures, StandardScaler
-from sklearn.metrics import mean_squared_error
-from sklearn.pipeline import make_pipeline
 from joblib import dump
 import os
 
